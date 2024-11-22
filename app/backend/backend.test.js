a3fd3dd2ec9aa0509210f1ffe69886b9d5f9964f
VictorCortez358 <00022120@uca.edu.sv>
2024-11-22 10:09:40 -0600

    prueba

--- app/backend/backend.test.js
+++ app/backend/backend.test.js
@@ -1,5 +1,5 @@
-import request from 'supertest';
-import app from './server';
+const request = require('supertest');
+const app = require('./server');
 
 describe('GET /', () => {
     it('should return a list of products', async () => {
