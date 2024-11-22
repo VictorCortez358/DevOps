618424d75f1c79a406c8f646ac90830001f65e4c
VictorCortez358 <00022120@uca.edu.sv>
2024-11-22 09:05:04 -0600

    Change node version

--- app/frontend/server.js
+++ app/frontend/server.js
@@ -1,5 +1,10 @@
-let express = require('express');
-let path = require('path');
+import express from 'express';
+import { join } from 'path';
+import { createRequire } from 'module';
+
+// Crear una referencia para usar 'require' dentro de ESM
+const require = createRequire(import.meta.url);
+const http = require('http'); // Ahora podemos usar require para http
 
 let app = express();
 
@@ -9,12 +14,12 @@ const backendEndpoints =
         : process.env.BACKEND_SERVICE_DEV;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    // Usamos import.meta.url para obtener el __dirname
+    const __dirname = new URL('.', import.meta.url).pathname;
+    res.sendFile(join(__dirname, "index.html"));
 });
 
 app.get('/get-products', function (req, res) {
-    var http = require('http');
-
     var options = {
         host: backendEndpoints,
         path: '/',
@@ -22,22 +27,22 @@ app.get('/get-products', function (req, res) {
         method: 'GET'
     };
 
-    callback = function(response) {
-        var str = ''
+    const callback = function(response) {
+        let str = '';
         response.on('data', function (chunk) {
             str += chunk;
         });
 
         response.on('end', function () {
             console.log(str);
-            res.writeHead(200)
+            res.writeHead(200);
             res.end(str);
         });
     }
 
-    var req = http.request(options, callback);
-    req.write("");
-    req.end();
+    var reqHttp = http.request(options, callback);
+    reqHttp.write("");
+    reqHttp.end();
 });
 
 app.listen(3000, () => {
