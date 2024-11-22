c0d55000bd61c1f99930af3d9133819c76fce828
VictorCortez358 <00022120@uca.edu.sv>
2024-11-21 22:01:06 -0600

    Fixed problems with the frontend server

--- app/frontend/frontend.test.js
+++ app/frontend/frontend.test.js
@@ -1,22 +1,8 @@
-import { chromium } from 'playwright';
+import { test, expect } from '@playwright/test';
 
-describe('Frontend tests', () => {
-    let browser;
-    let page;
-
-    beforeAll(async () => {
-        browser = await chromium.launch();
-        page = await browser.newPage();
-    });
-
-    afterAll(async () => {
-        await browser.close();
-    });
-
-    it('should display products when clicking the View button', async () => {
-        await page.goto('http://localhost:3000'); 
-        await page.click('button'); 
-        const products = await page.$$('ul#products .product');
-        expect(products.length).toBeGreaterThan(0);
-    });
+test('should display products when clicking the View button', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.click('button');
+    const products = await page.$$('ul#products .product');
+    expect(products.length).toBeGreaterThan(0);
 });
