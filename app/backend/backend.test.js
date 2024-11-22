12c4e7cfe059efca99961a6f413e58d679a97266
VictorCortez358 <00022120@uca.edu.sv>
2024-11-22 09:58:23 -0600

    prueba

--- app/backend/backend.test.js
+++ app/backend/backend.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
-const app = require('./backend');
+const app = require('./server');
+
+
 
 describe('GET /', () => {
     it('should return a list of products', async () => {
